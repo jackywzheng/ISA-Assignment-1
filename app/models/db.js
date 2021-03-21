bcc5bee954c2d141de56add6d1a8d093034af57c
Jacky Zheng <30736593+jackywzheng@users.noreply.github.com>
2021-03-20 22:30:38 -0700

    change mySQL connection to pool

--- app/models/db.js
+++ app/models/db.js
@@ -1,29 +1,11 @@
 const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 
-const con = mysql.createConnection({
+const con = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB
-});
-
-con.connect(err => {
-  if (err) {
-      throw err;
-    };
-  con.query(`CREATE TABLE IF NOT EXISTS Question (
-      id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-      content VARCHAR(100) NOT NULL,
-      answer_1 VARCHAR(100) NOT NULL,
-      answer_2 VARCHAR(100) NOT NULL,
-      answer_3 VARCHAR(100) NOT NULL,
-      answer_4 VARCHAR(100) NOT NULL,
-      correct VARCHAR(100) NOT NULL);`, (err, result) => {
-        if (err) throw err;
-        console.log('Table created')
-      })
-  console.log("Successfully connected to the database.");
 });
 
 module.exports = con;
